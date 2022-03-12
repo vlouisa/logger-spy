a5ebe2e11a34c0b5775518c7b71a7d8404b54642
Bouke Nijhuis <bouke.nijhuis@gmail.com>
2022-03-12 17:49:42 +0100

    smaller matchers

--- src/test/java/dev/louisa/LoggerSpyTest.java
+++ src/test/java/dev/louisa/LoggerSpyTest.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Test;
 
 import java.util.regex.Pattern;
 
-import static dev.louisa.LoggerSpyMatcher.*;
-import static dev.louisa.LoggerSpyMatcher.equalTo;
-import static dev.louisa.LoggerSpyMatcher.startsWith;
-import static java.util.regex.Pattern.*;
+import static dev.louisa.LoggerSpyMatcher.MatchPredicate.equalTo;
+import static dev.louisa.LoggerSpyMatcher.MatchPredicate.matches;
+import static dev.louisa.LoggerSpyMatcher.MatchPredicate.startsWith;
+import static java.util.regex.Pattern.compile;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
@@ -24,41 +24,41 @@ public class LoggerSpyTest {
 
     @Test
     void should_inform_that_a_line_has_been_logged() {
-        log.info("Start task");  
-        assertThat(loggerSpy.containsMessage(equalTo("Start task"))).isTrue();    
+        log.info("Start task");
+        assertThat(loggerSpy.containsMessage(equalTo("Start task"))).isTrue();
     }
 
     @Test
     void should_inform_that_a_line_has_not_been_logged() {
-        log.info("Start task");  
-        assertThat(loggerSpy.containsMessage(equalTo("Stop task"))).isFalse();    
+        log.info("Start task");
+        assertThat(loggerSpy.containsMessage(equalTo("Stop task"))).isFalse();
     }
 
     @Test
     void should_inform_that_a_line_that_starts_with_a_given_string_has_been_logged() {
-        log.info("Start task");  
-        assertThat(loggerSpy.containsMessage(startsWith("Start"))).isTrue();    
+        log.info("Start task");
+        assertThat(loggerSpy.containsMessage(startsWith("Start"))).isTrue();
     }
 
     @Test
     void should_inform_that_a_line_that_starts_with_a_given_string_has_not_been_logged() {
-        log.info("Start task");  
-        assertThat(loggerSpy.containsMessage(startsWith("Stop"))).isFalse();    
+        log.info("Start task");
+        assertThat(loggerSpy.containsMessage(startsWith("Stop"))).isFalse();
     }
 
     @Test
     void should_inform_that_a_line_that_complies_to_a_regex_has_been_logged() {
-        log.info("Start task #13");  
-        
-        Pattern pattern = compile("Start task #[0-9]{2}"); 
-        assertThat(loggerSpy.containsMessage(matches(pattern))).isTrue();    
+        log.info("Start task #13");
+
+        Pattern pattern = compile("Start task #[0-9]{2}");
+        assertThat(loggerSpy.containsMessage(matches(pattern))).isTrue();
     }
 
     @Test
     void should_inform_that_no_line_has_been_looged_that_complies_to_a_given_regex() {
-        log.info("Start task Nr. 13");  
-        
-        Pattern pattern = compile("Start task #[0-9]{2}"); 
-        assertThat(loggerSpy.containsMessage(matches(pattern))).isFalse();    
+        log.info("Start task Nr. 13");
+
+        Pattern pattern = compile("Start task #[0-9]{2}");
+        assertThat(loggerSpy.containsMessage(matches(pattern))).isFalse();
     }
 }
